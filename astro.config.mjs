b5b137c3f265de9a5cfe1a366b30c7dc4a89bd7d
Israni, Murli <murli.p.israni@gmail.com>
2022-12-02 10:36:39 +0530

    misc

--- astro.config.mjs
+++ astro.config.mjs
@@ -22,15 +22,28 @@ import htmlBeautifier from "astro-html-beautifier";
 // https://astro.build/config
 
 // https://astro.build/config
+import preact from "@astrojs/preact";
+
+// https://astro.build/config
+import image from "@astrojs/image";
+
+// https://astro.build/config
+import prefetch from "@astrojs/prefetch";
+
+// https://astro.build/config
 export default defineConfig({
+  // devOptions: {
+  //   tailwindConfig: './tailwind.config.js',
+  // },
   markdown: {
     drafts: true
   },
   integrations: [tailwind({
     config: {
-      applyBaseStyles: false
+      applyBaseStyles: false,
+      path: './tailwind.config.cjs'
     }
   }), mdx({
     drafts: true
-  }), react(), svelte(), htmlBeautifier()]
+  }), react(), svelte(), htmlBeautifier(), preact(), image(), prefetch()]
 });
